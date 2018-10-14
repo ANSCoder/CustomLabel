a8b9884d482d50de21ed74a341099172f00ec7d2
ANSCoder <nimjea@gmail.com>
2018-10-14 22:51:50 +0530

    Updated code for Swift 4.2.

--- CustomLabel/Extension/ExUILabel.swift
+++ CustomLabel/Extension/ExUILabel.swift
@@ -12,18 +12,18 @@ extension UILabel{
     
     func makeOutLine(oulineColor: UIColor, foregroundColor: UIColor){
         let strokeTextAttributes = [
-            NSAttributedStringKey.strokeColor : oulineColor,
-            NSAttributedStringKey.foregroundColor : foregroundColor,
-            NSAttributedStringKey.strokeWidth : -4.0,
-            NSAttributedStringKey.font : self.font
-            ] as [NSAttributedStringKey : Any]
+            NSAttributedString.Key.strokeColor : oulineColor,
+            NSAttributedString.Key.foregroundColor : foregroundColor,
+            NSAttributedString.Key.strokeWidth : -4.0,
+            NSAttributedString.Key.font : self.font
+            ] as [NSAttributedString.Key : Any]
         self.attributedText = NSMutableAttributedString(string: self.text ?? "", attributes: strokeTextAttributes)
     }
     
     func underline() {
         if let textString = self.text {
             let attributedString = NSMutableAttributedString(string: textString)
-            attributedString.addAttribute(NSAttributedStringKey.underlineStyle, value: NSUnderlineStyle.styleSingle.rawValue, range: NSRange(location: 0, length: attributedString.length))
+            attributedString.addAttribute(NSAttributedString.Key.underlineStyle, value: NSUnderlineStyle.single.rawValue, range: NSRange(location: 0, length: attributedString.length))
             attributedText = attributedString
         }
     }
